4c03b312dbabf264a6e21ea7bdc114893e323b5e
GUOAB <AbbyGuo@oocl.com>
2018-07-11 16:42:33 +0800

    TDD add parking UnitTest

---
+++ src/main/java/com/thoughtworks/tdd/Park.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.tdd;
+
+public class Park {
+    int size;
+    public Park(int size) {
+        this.size = size;
+    }
+
+    public boolean parking(Car car) {
+        return true;
+    }
+}
---
+++ src/test/java/com/thoughtworks/tdd/ParkinglogTest.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ParkinglogTest {
+    @Test
+    public void should_return_true_when_call_paking_given_input_Car(){
+        Car car = new Car();
+        Park park = new Park(1);
+        boolean result = park.parking(car);
+        assertThat(result, is(true));
+    }
+}
