98bb720b595f58b0ad9686fe9b36c7f5a1790c5a
GUOAB <AbbyGuo@oocl.com>
2018-07-12 15:10:46 +0800

    modify

--- src/test/java/com/thoughtworks/tdd/ParkinglogTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkinglogTest.java
@@ -86,10 +86,8 @@ public class ParkinglogTest {
     @Test
     public void should_be_successfully_given_parking_lot_is_not_full(){
         ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(1);
         List<ParkingLot> pakingLotList = new ArrayList<>();
         pakingLotList.add(parkingLot1);
-        pakingLotList.add(parkingLot2);
         ParkingBoy parkingBoy = new ParkingBoy(pakingLotList);
         try {
             parkingBoy.parking(new Car());
@@ -101,13 +99,12 @@ public class ParkinglogTest {
 
     @Test
     public void should_be_failed_given_parking_lot_is_full(){
-        ParkingLot parkingLot1 = new ParkingLot(0);
-        ParkingLot parkingLot2 = new ParkingLot(0);
+        ParkingLot parkingLot1 = new ParkingLot(1);
         List<ParkingLot> pakingLotList = new ArrayList<>();
         pakingLotList.add(parkingLot1);
-        pakingLotList.add(parkingLot2);
         ParkingBoy parkingBoy = new ParkingBoy(pakingLotList);
         try {
+            parkingBoy.parking(new Car());
             parkingBoy.parking(new Car());
         } catch (ParkingLotFullException exception) {
             fail("should park successfully");
@@ -126,6 +123,37 @@ public class ParkinglogTest {
         try {
             parkingBoy.parking(new Car());
             parkingBoy.parking(new Car());
+        } catch (ParkingLotFullException exception) {
+            fail("should park successfully");
+        }
+
+    }
+
+    @Test
+    public void should_get_specific_car_when_call_unPark_given_receipt_is_right_by_parkingBoy(){
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        List<ParkingLot> pakingLotList = new ArrayList<>();
+        pakingLotList.add(parkingLot1);
+        ParkingBoy parkingBoy = new ParkingBoy(pakingLotList);
+        Car theCar = new Car();
+        Receipt receipt = parkingBoy.parking(theCar);
+        assertThat(parkingBoy.unPark(receipt), is(theCar));
+
+    }
+
+    @Test
+    public void should_get_specific_car_when_call_unPark_more_given_receipt_is_right_by_parkingBoy(){
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(1);
+        List<ParkingLot> pakingLotList = new ArrayList<>();
+        pakingLotList.add(parkingLot1);
+        pakingLotList.add(parkingLot2);
+        ParkingBoy parkingBoy = new ParkingBoy(pakingLotList);
+        try {
+            Receipt receipt = parkingBoy.parking(new Car());
+            Receipt receipt1 = parkingBoy.parking(new Car());
+            parkingBoy.unPark(receipt);
+            parkingBoy.unPark(receipt1);
         } catch (ParkingLotFullException exception) {
             fail("should park successfully");
         }
@@ -152,16 +180,6 @@ public class ParkinglogTest {
 
     }
 
-    @Test
-    public void should_get_specific_car_when_call_unPark_given_receipt_is_right_by_parkingBoy(){
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        List<ParkingLot> pakingLotList = new ArrayList<>();
-        pakingLotList.add(parkingLot1);
-        ParkingBoy parkingBoy = new ParkingBoy(pakingLotList);
-        Car theCar = new Car();
-        Receipt receipt = parkingBoy.parking(theCar);
-        assertThat(parkingBoy.unPark(receipt), is(theCar));
 
-    }
 
 }
